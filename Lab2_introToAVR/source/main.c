7583c4bcb6ce329e455334d369d894c5a9b8f371
Christian Melendez <cmele014@wch136-19.cs.ucr.edu>
2020-04-15 20:13:02 -0700

    Finished 'Accessing individual pins (bit masking)' section

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -15,11 +15,18 @@
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
-				// Initialize output on PORTB to 0x00
-    unsigned char temp_input = 0x00;
+    unsigned char tmpB = 0x00; //Temporary variable to hold the value of B
+    unsigned char tmpA = 0x00; //Temporary variable to hold the value of A
+
     while(1) {
-        temp_input = PINA;
-        PORTB = temp_input;	// Writes port B's 8 pins with the values					// on port A's 8 pins
+        tmpA = PINA & 0x01;
+        
+	if(tmpA == 0x01) {
+	    tmpB = (tmpB & 0xFC) | 0x01;
+        } else {
+	    tmpB = (tmpB & 0xFC) | 0x02;
+	}
+	PORTB = tmpB;
     }
-    return 1;
+    return 0;
 }
