3f415e0641aa67ea50430a2dc6d8b4ba9195fbf8
KurvTurtle <kurvturtle@gmail.com>
2020-04-26 22:13:01 -0700

    Complete exercise 2 code, need to demo

--- Lab5_ATMega1284P/source/main.c
+++ Lab5_ATMega1284P/source/main.c
@@ -1,8 +1,8 @@
 /*	Author: Christian Melendez
  *  Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #5  Exercise #1
- *	DEMONSTRATION LINK: https://drive.google.com/open?id=1JYUnkMzTo7_31C4GzdKh9jrp5qTDN8Wj
+ *	Assignment: Lab #5  Exercise #2
+ *	DEMONSTRATION LINK: https://drive.google.com/
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,140 +14,77 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States{L0, L1, L2, L3, L4, L5, L6} state;
+enum States{Init, Increment, Decrement, Pressed, Reset} state;
 
-void fuelLevels() {
-	unsigned char input = ~PINA & 0x0F;
+void incrementDecrement() {
+	unsigned char input = ~PINA & 0x03; //Read A0 and A1
 	
 	switch(state) { //Transitions
-		case L0: //Initial State
+		case Init: //Initial State
 			if (input == 0x00) {
-				state = L0;
-                       } else if (input == 0x01 | input == 0x02) {
-		                state = L1;
-		        } else if (input == 0x03 | input == 0x04) {
-                		state = L2;
-		        } else if (input == 0x05 | input == 0x06) {
-                		state = L3;
-		        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                		state = L4;
-		        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                		state = L5;
-		        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                		state = L6;
-		        }
+				state = Init;
+			} else if (input == 0x01) {
+				state = Increment;
+			} else if (input == 0x02) {
+				state = Decrement;
+			} else if (input == 0x03) {
+				state = Reset;
+			}
 			break;
-		case L1:
-			 if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
+		case Increment:
+			state = Pressed;
 			break;
-		case L2:  if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
+		case Decrement:
+			state = Pressed;
 			break;
-		case L3:
-		       if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
-                        break;
-		case L4:
+		case Pressed:
 			if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
-                        break;
-		case L5:
-			if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
-                        break;
-		case L6:
-			if (input == 0x00) {
-                                state = L0;
-                        } else if (input == 0x01 | input == 0x02) {
-                                state = L1;
-                        } else if (input == 0x03 | input == 0x04) {
-                                state = L2;
-                        } else if (input == 0x05 | input == 0x06) {
-                                state = L3;
-                        } else if (input == 0x07 | input == 0x08 | input == 0x09) {
-                                state = L4;
-                        } else if (input == 0x0A | input == 0x0B | input == 0x0C) {
-                                state = L5;
-                        } else if (input == 0x0D | input == 0x0E | input == 0x0F) {
-                                state = L6;
-                        }
-                        break;
+				state = Init;
+			} else if (input == 0x03) {
+				state = Reset;
+			}
+			break;
+		case Reset:
+			state = Init;
+			break;
 	} //Transitions
 
 	switch(state) { //State actions
-		case L0: PORTC = 0x40; break; //Low fuel
-		case L1: PORTC = 0x60; break; //Low fuel
-		case L2: PORTC = 0x70; break; //Low fuel
-		case L3: PORTC = 0x38; break;
-		case L4: PORTC = 0x3C; break;
-		case L5: PORTC = 0x3E; break;
-		case L6: PORTC = 0x3F; break;
+		case Init: 
+			break;
+		case Increment: 
+			switch(PORTC) { //Increment
+                                case 0x00: PORTC = 0x01; break;
+                                case 0x01: PORTC = 0x02; break;
+                                case 0x02: PORTC = 0x03; break;
+                                case 0x03: PORTC = 0x04; break;
+                                case 0x04: PORTC = 0x05; break;
+                                case 0x05: PORTC = 0x06; break;
+                                case 0x06: PORTC = 0x07; break;
+                                case 0x07: PORTC = 0x08; break;
+                                case 0x08: PORTC = 0x09; break;
+                                case 0x09: PORTC = 0x09; break;
+                        } //Increment
+		break;
+		case Decrement: 
+			switch(PORTC) { //Decrement
+				case 0x00: PORTC = 0x00; break;
+				case 0x01: PORTC = 0x00; break;
+				case 0x02: PORTC = 0x01; break;
+				case 0x03: PORTC = 0x02; break;
+				case 0x04: PORTC = 0x03; break;
+				case 0x05: PORTC = 0x04; break;
+				case 0x06: PORTC = 0x05; break;
+				case 0x07: PORTC = 0x06; break;
+				case 0x08: PORTC = 0x07; break;
+				case 0x09: PORTC = 0x08; break;
+			} //Decrement
+			break;
+		case Pressed:
+			break;
+		case Reset:
+			PORTC = 0x00;
+			break;
 	} //State actions
 }
 
@@ -157,11 +94,11 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs
     DDRC = 0xFF; PORTC = 0x00; //Configure port C's 8 pins as outputs
 
-    state = L0;
+    state = Init; //Set initial state
+    PORTC = 0x00; //Initial value of PORTC
 
     while (1) {
-	PORTC = 0x00;
-	fuelLevels();
+	incrementDecrement();
     }
     return 1;
 }
