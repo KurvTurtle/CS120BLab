e7131d68b4b443f02a64433194a70a0a80b76a5e
KurvTurtle <kurvturtle@gmail.com>
2020-05-16 22:54:55 -0700

    Created labs 8, 9, 10, and 11

---
+++ Lab8_ADC/source/main.c
@@ -0,0 +1,39 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init() { 
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+
+	//ADEN: Setting this bit enables analog-to-digital conversion
+	//ADSC: Setting this bit starts the first conversion
+	//ADATE: Setting this bit enables auto-triggering
+}
+
+void main() {
+    DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0xFF; //Configure port B's 8 pins as outputs
+    DDRD = 0xFF; PORTD = 0x00; //Configure port D's 8 pins as outputs
+
+    ADC_init(); //Initializes the ADC
+    
+    PORTB = 0x00; //Initial value of PORTB
+    PORTD = 0x00; //Initial value of PORTD
+
+    while (1) { 
+	//User Code
+	unsigned short input = ADC; 
+	PORTB = (char)input & 0xFF;
+	PORTD = (char)input >> 8;
+	}    
+}
